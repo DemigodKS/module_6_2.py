098b296051d338006b01f2fb8b740f0b62a6cc67
DemigodKS <mankok222@gmail.com>
2024-10-22 18:18:13 +0300

    Update module_6_2.py

--- module_6_2.py
+++ module_6_2.py
@@ -6,12 +6,12 @@ class Vehicle:
         self.__engine_power = __engine_power
         self.__color = __color
 
-    def get_model(self):
-        return (f'Модель: {self.__model}')
-    def get_horsepower(self):
-        return (f'Мощность двигателя: {self.__engine_power}')
-    def get_color(self):
-        return (f'Цвет: {self.__color}')
+    #def get_model(self):
+        #return (f'Модель: {self.__model}')
+    #def get_horsepower(self):
+        #return (f'Мощность двигателя: {self.__engine_power}')
+    #def get_color(self):
+        #return (f'Цвет: {self.__color}')
     def print_info(self):
         print(f'Модель: {self.__model}')
         print(f'Мощность двигателя: {self.__engine_power}')
